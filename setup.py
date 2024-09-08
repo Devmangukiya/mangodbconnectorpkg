79804aa7fd827e4d0bbabdc90096bd9fac3c1e5d
Dev Mangukiya <devmangukiya561@gmail.com>
2024-09-08 09:06:37 +0530

    dere

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.md', 'r' , encoding='utf-8') as f:
 
 __version__ = "0.0.4"
 REPO_NAME = "mangodbconnectorpkg"
-PKG_NAME = "Mongo-Connect"
+PKG_NAME = "MongoConnectorPackage"
 AUTHOR_USER_NAME = "Devmangukiya"
 AUTHOR_EMAIL = "devmangukiya561@gmail.com"
 
